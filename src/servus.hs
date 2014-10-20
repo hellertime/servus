3a32c753ec3dceb9a8ecf500255e3c1af462c397
Chris Heller <hellertime@gmail.com>
2014-10-19 23:59:56 -0400

    Scheduler mixes IO and STM ... hmm

--- src/servus.hs
+++ src/servus.hs
@@ -1,36 +1,59 @@
 module Main where
 
 import           Control.Applicative
+import           Control.Monad
 import           Control.Monad.STM
 import           Control.Monad.Loops.STM
 import           Control.Concurrent
 import           Control.Concurrent.STM.TMVar
 import           Data.List
+import           Data.Monoid ((<>))
 import           Debug.Trace
+import           System.Mesos.Scheduler
 
 -- | Drive the main scheduler loop. The server continuously will
 -- pull out of the set of expired jobs any which are requesting to
 -- be scheduled again. The remaining expired jobs are logged out and
 -- discarded.
-schedulerLoop s = atomLoop $ do
-    traceM "entering scheduler loop"
-    (pendingJobs, expiredJobs) <- partitionExpiredJobs
-    schedulePendingJobs pendingJobs
-    traceM "scheduled"
+schedulerLoop s = do
+    expiredJobs <- tryTakeTMVar (_sExpiredJobs s)
+    readyJobs   <- tryTakeTMVar (_sReadyJobs s)
+    putTMVar (_sReadyJobs) readyJobs'
   where
-    partitionExpiredJobs = takeTMVar (expiredJobs s) >>= \e -> return $ partition nextReadyJob e
-    schedulePendingJobs pendingJobs = do
-        r <- takeTMVar (readyJobs s) `orElse` return []
-        traceM ("Ready Jobs: " ++ show r)
-        traceM ("Pending Jobs: " ++ show pendingJobs)
-        putTMVar (readyJobs s) (r ++ pendingJobs)
+    (pendingJobs, expiredJobs') = maybe ([],[]) (partition nextReadyJob) expiredJobs
+    readyJobs' =  maybe pendingJobs (<> pendingJobs) readyJobs
     nextReadyJob = (== "a")
 
 data Servus = Servus
-    { readyJobs   :: TMVar [String]
-    , activeJobs  :: TMVar [String]
-    , expiredJobs :: TMVar [String]
+    { _sReadyJobs   :: TMVar [String]
+    , _sActiveJobs  :: TMVar [String]
+    , _sExpiredJobs :: TMVar [String]
     }
+
+data ServusScheduler = ServusScheduler
+    { _ssServus :: Servus
+    }
+
+instance ToScheduler ServusScheduler where
+    registered _ _ _ _ = putStrLn "servus: registered"
+
+    resourceOffers s driver offers = do
+        forM_ offers $ \offer -> atomically $ do
+            case tryTakeTMVar (_sReadyJobs servus) of
+                Nothing        -> declineOffer driver (offerID offer) filters
+                Just readyJobs -> do
+                    status <- launchTasks
+                        driver
+                        [ offerID offer ]
+                        [ TaskInfo "Tasker" (TaskID "task") (offerSlaveID offer) requiredResources
+                            (TaskExecutor $ executorSettings $ offerFrameworkID offer)
+                            Nothing
+                            Just $ CommandInfo [] Nothing "ps -ef" Nothing
+                            Nothing
+                        ]
+                        filters
+      where
+        servus = (_ssServus s)
 
 main = do
     servus <- Servus 
@@ -39,12 +62,10 @@ main = do
               <*> newEmptyTMVarIO
 
     forkAtomLoop $ do
-        traceM "entering framework loop"
-        r <- takeTMVar (readyJobs servus)
-        e <- takeTMVar (expiredJobs servus) `orElse` return []
-        traceM ("Run Jobs: " ++ show r)
-        traceM ("Expired Jobs: " ++ show e)
-        putTMVar (expiredJobs servus) (e ++ r)
-        traceM "Done"
+        readyJobs   <- takeTMVar $ _sReadyJobs servus
+        expiredJobs <- tryTakeMVar $ _sExpiredJobs servus
+        putTMVar (_sExpiredJobs servus) expiredJobs'
+      where
+        expiredJobs' = maybe readyJobs (<> readyJobs) expiredJobs
 
-    schedulerLoop servus
+    atomLoop $ schedulerLoop servus
