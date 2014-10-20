9540968b320721db7afb20deb6bd2a488626e81f
Chris Heller <hellertime@gmail.com>
2014-10-20 06:41:07 -0400

    Add more precise types

--- src/servus.hs
+++ src/servus.hs
@@ -7,7 +7,8 @@ import           Control.Monad.Loops.STM
 import           Control.Concurrent
 import           Control.Concurrent.STM.TMVar
 import           Data.List
-import           Data.Monoid ((<>))
+import           Data.Map                     as M
+import           Data.Monioid ((<>))
 import           Debug.Trace
 import           System.Mesos.Scheduler
 
@@ -21,14 +22,39 @@ schedulerLoop s = do
     putTMVar (_sReadyJobs) readyJobs'
   where
     (pendingJobs, expiredJobs') = maybe ([],[]) (partition nextReadyJob) expiredJobs
-    readyJobs' =  maybe pendingJobs (<> pendingJobs) readyJobs
+    readyJobs' =  maybe pendingJobs (++ pendingJobs) readyJobs
     nextReadyJob = (== "a")
 
-data Servus = Servus
-    { _sReadyJobs   :: TMVar [String]
-    , _sActiveJobs  :: TMVar [String]
-    , _sExpiredJobs :: TMVar [String]
+data ServusJob = ServusJob
+    { _sjName :: String
     }
+
+data Servus = Servus
+    { _sReadyJobs   :: TMVar [ServusJob]
+    , _sActiveJobs  :: TMVar (M.Map TaskID ServusJob)
+    , _sExpiredJobs :: TMVar [ServusJob]
+    }
+
+satisfyOffer :: Servus -> Offer -> STM (Maybe ServusJob)
+satisfyOffer s _ = case takeTMVar (_sReadyJobs s) of
+    []     -> return Nothing
+    (j:js) -> putTMVar (_sReadyJobs s) js >> return $ Just j
+
+putActiveJob :: Servus -> ServusJob -> STM ()
+putActiveJob s j = do
+    activeJobs <- takeTMVar (_sActiveJobs s)
+    putTMVar (_sActiveJobs s) (j : activeJobs)
+
+takeActiveJob :: Servus -> TaskID -> STM (Maybe ServusJob)
+takeActiveJob s tid = atomically $ case takeTMVar (_sActiveJobs s) of
+    [] -> return Nothing
+    (
+
+expireJob :: Servus -> TaskID -> STM ServusJob
+expireJob s tid = do
+    case takeTMVar (_sActiveJobs s) of
+        [] -> putTMVar
+
 
 data ServusScheduler = ServusScheduler
     { _ssServus :: Servus
@@ -38,34 +64,46 @@ instance ToScheduler ServusScheduler where
     registered _ _ _ _ = putStrLn "servus: registered"
 
     resourceOffers s driver offers = do
-        forM_ offers $ \offer -> atomically $ do
-            case tryTakeTMVar (_sReadyJobs servus) of
+        forM_ offers $ \offer -> do
+            case atomically $ tryTakeTMVar (_sReadyJobs servus) of
                 Nothing        -> declineOffer driver (offerID offer) filters
                 Just readyJobs -> do
+                    readyJob <- atomically $ satisfyOffer servus offer
                     status <- launchTasks
                         driver
                         [ offerID offer ]
-                        [ TaskInfo "Tasker" (TaskID "task") (offerSlaveID offer) requiredResources
+                        [ TaskInfo (_sjName readyJob) (TaskID "task") (offerSlaveID offer) requiredResources
                             (TaskExecutor $ executorSettings $ offerFrameworkID offer)
                             Nothing
                             Just $ CommandInfo [] Nothing "ps -ef" Nothing
                             Nothing
                         ]
                         filters
+                    atomically $ putActiveJob readyJob
+        return ()
+      where
+        servus = (_ssServus s)
+
+    statusUpdate s driver status = do
+        when (taskStatusState status == Finished) $ do
+            atomically $ do
+                expiredJobs <- takeTMVar (_sExpiredJobs servus)
+                activeJobs  <-
+                
       where
         servus = (_ssServus s)
 
 main = do
     servus <- Servus 
               <$> (newTMVarIO ["a","b","c"])
-              <*> newEmptyTMVarIO 
-              <*> newEmptyTMVarIO
+              <*> (newTMVarIO M.emptyMap)
+              <*> (newTMVarIO [])
 
     forkAtomLoop $ do
         readyJobs   <- takeTMVar $ _sReadyJobs servus
         expiredJobs <- tryTakeMVar $ _sExpiredJobs servus
         putTMVar (_sExpiredJobs servus) expiredJobs'
       where
-        expiredJobs' = maybe readyJobs (<> readyJobs) expiredJobs
+        expiredJobs' = maybe readyJobs (++ readyJobs) expiredJobs
 
     atomLoop $ schedulerLoop servus
