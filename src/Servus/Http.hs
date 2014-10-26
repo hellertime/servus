d37c81250ac6683826a24f189f460a81f2a20869
Chris Heller <hellertime@gmail.com>
2014-10-26 01:54:17 -0400

    Make proper toJSON calls.

--- src/Servus/Http.hs
+++ src/Servus/Http.hs
@@ -2,32 +2,37 @@
 
 module Servus.Http where
 
-import Control.Concurrent.STM
-import Control.Monad.Reader
-import Data.Text.Lazy                        (Text)
-import Network.Wai.Middleware.RequestLogger
-import Web.Scotty.Trans
+import           Control.Concurrent.STM
+import           Control.Monad.Reader
+import           Data.Aeson
+import qualified Data.Map.Strict                     as M
+import           Data.Monoid                                 ((<>))
+import           Data.Text.Lazy                              (Text, empty)
+import           Network.Wai.Middleware.RequestLogger
+import           Web.Scotty.Trans
+import qualified Web.Scotty.Trans                     as WST
 
+import Servus.Config
 import Servus.Server
 import Servus.Task
 
 newtype HalTaskLibrary = HalTaskLibrary TaskLibrary
 
 instance ToJSON HalTaskLibrary where
-    toJSON (HalTaskLibrary tl) = object $ [ "_links" .= hal ]
+    toJSON (HalTaskLibrary (TaskLibrary tl)) = object [ "_links" .= hal ]
       where
-        hal = object $ [ "self"          .= object $ [ "href" .= "/" ]
-                       , "curies"        .= toJSON curies
-                       , "x:taskLibrary" .= toJSON tasks
+        hal = object $ [ "self"          .= object [ "href" .= ("/" <> empty) ]
+                       , "curies"        .= curies
+                       , "x:taskLibrary" .= tasks
                        ]
-        curies = [ object $ [ "name" .= "x", "href" .= "xxx", "templated" .= toJSON True ] ]
-        tasks = map toLink $ M.keys tl
-        toLink (name, Nothing)    = object $ [ "href" .= "/taskLibrary/system/" <> name ]
-        toLink (name, Just owner) = object $ [ "href" .= "/taskLibrary/user/" <> owner <> "/" <> name ]
+        curies = [ object [ "name" .= ("x" <> empty), "href" .= ("xxx" <> empty), "templated" .= True ] ]
+        tasks = object $ map toLink $ M.keys tl
+        toLink (name, SystemTask)    = "href" .= ("/taskLibrary/system/" <> name)
+        toLink (name, UserTask owner) = "href" .= ("/taskLibrary/user/" <> owner <> "/" <> name)
 
 restApi :: ScottyT Text TaskM ()
 restApi = do
     middleware logStdoutDev
     get "/" $ do
-       library <- taskM $ ask >>= _library . readTVar
-       json library
+       library <- taskM $ ask >>= liftIO . atomically . readTVar . _library
+       WST.json $ HalTaskLibrary library
