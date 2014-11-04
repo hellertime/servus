04ca3d0cfdc34d741438b30fed2e9e5b210a1535
Chris Heller <hellertime@gmail.com>
2014-11-04 16:56:54 -0500

    Qualified Mesos...

--- src/Servus/Task.hs
+++ src/Servus/Task.hs
@@ -11,13 +11,12 @@ import qualified Data.Map.Strict       as M
 import           Data.Maybe
 import           Data.RangeSpace
 import qualified Data.Text 	       as T
-import 		 Data.Text.Encoding             (encodeUtf8)
+import 		 Data.Text.Encoding             (encodeUtf8, decodeUtf8)
 import           Data.Time
 import           Data.UUID.V4                   (nextRandom)
 import           Data.UUID.V5                   (generateNamed)
 import qualified Data.UUID             as UUID
-import           System.Mesos.Types             (TaskInfo (..), TaskID (..), ExecutorID (..), ExecutorInfo (..))
-import qualified System.Mesos.Types    as MT
+import qualified System.Mesos.Types    as MZ
 
 import           Servus.Config
 
@@ -33,8 +32,8 @@ data TaskSchedule = TaskSchedule
 data Task a = Task 
     { _tConf   :: TaskConf
     , _tSched  :: TaskSchedule
-    , _tInfo   :: MT.TaskInfo
-    , _tStatus :: Maybe MT.TaskStatus
+    , _tInfo   :: MZ.TaskInfo
+    , _tStatus :: Maybe MZ.TaskStatus
     }
   deriving (Show, Eq)
 
@@ -47,11 +46,11 @@ data Running
 data Finished
 
 -- | Build and return a new task instance, based on a config and an id
-newTask :: TaskConf -> MT.TaskID -> IO (Task Ready)
+newTask :: TaskConf -> MZ.TaskID -> IO (Task Ready)
 newTask conf tid = getCurrentTime >>= \t -> return $ Task conf (sched {_tsReadyTime = t}) info Nothing
   where
     sched = TaskSchedule Nothing (UTCTime (ModifiedJulianDay 0) (secondsToDiffTime 0))
-    info  = MT.TaskInfo { taskInfoName       = encodeUtf8 $ _tcName conf
+    info  = MZ.TaskInfo { taskInfoName       = encodeUtf8 $ _tcName conf
                         , taskID             = tid
                         , taskSlaveID        = "" -- ^ must populate strict fields
                         , taskResources      = map fromResource (maybe [] _rsrcs $ _tcResources conf)
@@ -62,49 +61,49 @@ newTask conf tid = getCurrentTime >>= \t -> return $ Task conf (sched {_tsReadyT
                         }
 
 -- | Run a task, requires a SlaveID and a FrameworkID
-runTask :: Task Ready -> MT.FrameworkID -> MT.SlaveID -> Task Running
+runTask :: Task Ready -> MZ.FrameworkID -> MZ.SlaveID -> Task Running
 runTask task fid sid = task'
   where
     task' = task { _tInfo = info' }
     info  = _tInfo task
-    info' = info { taskSlaveID        = sid
-                 , taskImplementation = taskImplementation'
+    info' = info { MZ.taskSlaveID        = sid
+                 , MZ.taskImplementation = taskImplementation'
                  }
-    taskImplementation' = case (taskImplementation info) of
-                              (MT.TaskExecutor ei) -> MT.TaskExecutor $ ei { executorInfoFrameworkID = fid }
+    taskImplementation' = case (MZ.taskImplementation info) of
+                              (MZ.TaskExecutor ei) -> MZ.TaskExecutor $ ei { MZ.executorInfoFrameworkID = fid }
                               ci                   -> ci
 
 -- | Complete a task, and update its final status
-finishTask :: Task Running -> MT.TaskStatus -> Task Finished
+finishTask :: Task Running -> MZ.TaskStatus -> Task Finished
 finishTask task status = task { _tStatus = Just status } 
 
-fromResource :: (T.Text, Resource) -> MT.Resource
+fromResource :: (T.Text, Resource) -> MZ.Resource
 fromResource = uncurry go
   where
-    go name value = MT.Resource { resourceName = encodeUtf8 name, resourceValue = go' value, resourceRole = Nothing }
-    go' (Scalar n)  = MT.Scalar n
-    go' (Ranges rs) = MT.Ranges $ map (fromIntegral *** fromIntegral) rs
-    go' (Set    s)  = MT.Set    $ map encodeUtf8 s
-    go' (Text   t)  = MT.Text   $ encodeUtf8 t
+    go name value = MZ.Resource { resourceName = encodeUtf8 name, resourceValue = go' value, resourceRole = Nothing }
+    go' (Scalar n)  = MZ.Scalar n
+    go' (Ranges rs) = MZ.Ranges $ map (fromIntegral *** fromIntegral) rs
+    go' (Set    s)  = MZ.Set    $ map encodeUtf8 s
+    go' (Text   t)  = MZ.Text   $ encodeUtf8 t
 
-fromCommandConf :: MT.TaskID -> CommandConf -> MT.TaskExecutionInfo
+fromCommandConf :: MZ.TaskID -> CommandConf -> MZ.TaskExecutionInfo
 fromCommandConf tid = go
   where
-    go cc | isJust $ _ccExecutor cc = MT.TaskExecutor $ toExecutorInfo tid cc
-          | otherwise               = MT.TaskCommand  $ toCommandInfo cc
+    go cc | isJust $ _ccExecutor cc = MZ.TaskExecutor $ toExecutorInfo tid cc
+          | otherwise               = MZ.TaskCommand  $ toCommandInfo cc
 
-fromVolume :: Volume -> MT.Volume
+fromVolume :: Volume -> MZ.Volume
 fromVolume v = let volumeContainerPath = encodeUtf8 $ _volContainerPath v
                    volumeHostPath      = fmap encodeUtf8 $ _volHostPath v
-                   volumeMode          = if _volReadOnly v then MT.ReadOnly else MT.ReadWrite
-                in MT.Volume {..}
+                   volumeMode          = if _volReadOnly v then MZ.ReadOnly else MZ.ReadWrite
+                in MZ.Volume {..}
 
-toContainerInfo :: ContainerConf -> MT.ContainerInfo
-toContainerInfo (DockerConf image vols) = let containerInfoContainerType = MT.Docker $ encodeUtf8 image
+toContainerInfo :: ContainerConf -> MZ.ContainerInfo
+toContainerInfo (DockerConf image vols) = let containerInfoContainerType = MZ.Docker $ encodeUtf8 image
                                               containerInfoVolumes       = map fromVolume $ _vols vols
-                                          in MT.ContainerInfo {..}
+                                          in MZ.ContainerInfo {..}
 
-toExecutorInfo :: MT.TaskID -> CommandConf -> MT.ExecutorInfo
+toExecutorInfo :: MZ.TaskID -> CommandConf -> MZ.ExecutorInfo
 toExecutorInfo tid = go
   where
     go cc = let (Just executorConf)       = _ccExecutor cc
@@ -114,37 +113,41 @@ toExecutorInfo tid = go
                 executorInfoContainerInfo = fmap toContainerInfo $ _ccContainer cc
                 executorInfoResources     = map fromResource (maybe [] _rsrcs $ _execResources executorConf)
                 executorName              = Just $ encodeUtf8 $ _execName executorConf
-                executorSource            = Just $ MT.fromTaskID tid
+                executorSource            = Just $ MZ.fromTaskID tid
                 executorData              = Nothing
-            in MT.ExecutorInfo {..}
+            in MZ.ExecutorInfo {..}
 
 fromEnvVar :: (T.Text, T.Text) -> (C8.ByteString, C8.ByteString)
 fromEnvVar = encodeUtf8 *** encodeUtf8
 
-fromUri :: Uri -> MT.CommandURI
-fromUri (Uri val exec ext) = MT.CommandURI (encodeUtf8 val) exec ext
+fromUri :: Uri -> MZ.CommandURI
+fromUri (Uri val exec ext) = MZ.CommandURI (encodeUtf8 val) exec ext
 
-toCommandValue :: CommandSpec -> MT.CommandValue
-toCommandValue (ShellCommand cmd)      = MT.ShellCommand $ encodeUtf8 cmd
-toCommandValue (ExecCommand arg0 argv) = MT.RawCommand (encodeUtf8 arg0) $ map encodeUtf8 argv
+toCommandValue :: CommandSpec -> MZ.CommandValue
+toCommandValue (ShellCommand cmd)      = MZ.ShellCommand $ encodeUtf8 cmd
+toCommandValue (ExecCommand arg0 argv) = MZ.RawCommand (encodeUtf8 arg0) $ map encodeUtf8 argv
 
-toCommandInfo :: CommandConf -> MT.CommandInfo
+toCommandInfo :: CommandConf -> MZ.CommandInfo
 toCommandInfo cc = let commandInfoURIs    = map fromUri (maybe [] _uris $ _ccUris cc)
                        commandEnvironment = fmap (map fromEnvVar . _evs) $ _ccEnv cc
                        commandValue       = toCommandValue $ _ccRun cc
                        commandUser        = fmap encodeUtf8 $ _ccUser cc
-                    in MT.CommandInfo {..}
+                    in MZ.CommandInfo {..}
 
 -- | Generate a TaskID using a human hash of a random UUID
-randomTaskID :: TaskConf -> IO TaskID
-randomTaskID tc = nextRandom >>= return . TaskID . textEncode . taskIdGen
+randomTaskID :: TaskConf -> IO MZ.TaskID
+randomTaskID tc = nextRandom >>= return . MZ.TaskID . textEncode . taskIdGen
   where
     textEncode = encodeUtf8 . T.append (flip T.snoc '.' $ _tcName tc) . T.intercalate "_"
     taskIdGen = humanHash 5 . UUID.toString
 
+-- | Get the name of a task from its TaskID
+getNameTaskID :: MZ.TaskID -> TaskName
+getNameTaskID = fst . T.breakOn "." . decodeUtf8 . MZ.fromTaskID
+
 -- | Generate a ExecutorID deterministcally
-generateExecutorID :: ExecutorConf -> ExecutorID
-generateExecutorID cc = ExecutorID $ textEncode $ executorIdGen $ executorUuid
+generateExecutorID :: ExecutorConf -> MZ.ExecutorID
+generateExecutorID cc = MZ.ExecutorID $ textEncode $ executorIdGen $ executorUuid
   where
     (Just executorUuidNamespace) = UUID.fromString "e2b82c48-fb4b-40e8-bff7-8daf0424b0d9"
     textEncode = encodeUtf8 . T.append (flip T.snoc '.' $ _execName cc) . T.intercalate "_"
